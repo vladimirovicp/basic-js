4e3466429867b61646a08b624cc8b07cf7e6d494
vladimirovicp <vladimirovicp70@gmail.com>
2023-02-28 10:54:49 +0400

    start: 10 Шифр Виженера

---
+++ src/vigenere-cipher-main.js
@@ -0,0 +1,77 @@
+//const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Implement class VigenereCipheringMachine that allows us to create
+ * direct and reverse ciphering machines according to task description
+ * 
+ * @example
+ * 
+ * const directMachine = new VigenereCipheringMachine();
+ * 
+ * const reverseMachine = new VigenereCipheringMachine(false);
+ * 
+ * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
+ * 
+ * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
+ * 
+ * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
+ * 
+ * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
+ * 
+ */
+
+const alfavit = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';  //26 букв
+let alfavit_code = [];
+
+for (let i=0; i<alfavit.length; i++){
+  alfavit_code[alfavit[i]] = i;
+}
+
+console.log(alfavit_code)
+
+
+
+class VigenereCipheringMachine {
+
+  constructor(reverse) {
+    this.reverse = reverse;
+  }
+
+  encrypt(message,key) {
+
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+
+    if( !message || !key){
+      throw new Error('Incorrect arguments!');
+    }
+
+
+
+
+
+
+
+    if (this.reverse === false){
+      console.log(this.reverse)
+    }
+
+  }
+  decrypt() {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+}
+
+// module.exports = {
+//   VigenereCipheringMachine
+// };
+
+
+const directMachine = new VigenereCipheringMachine();
+
+directMachine.encrypt()
+
+const reverseMachine = new VigenereCipheringMachine(false);
+
+reverseMachine.encrypt()
