a7d8643ad316884706cf88e9ea411852235f65d3
vladimirovicp <vladimirovicp70@gmail.com>
2023-10-29 14:39:29 +0400

    add: 14 Закодируйте строку

---
+++ src/encode-line-main.js
@@ -0,0 +1,31 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Given a string, return its encoding version.
+ *
+ * @param {String} str
+ * @return {String}
+ *
+ * @example
+ * For aabbbc should return 2a3bc
+ *
+ */
+function encodeLine(str) {
+
+    let count = 1;
+    let result = '';
+    for (let i=0; i < str.length; i++){
+        if(str[i] === str[i+1]){
+            count++;
+        } else{
+            result = count === 1 ? result + str[i] : result + count + str[i];
+            count = 1;
+        }
+    }
+    console.log(result);
+
+
+}
+
+
+encodeLine('aabbbc');
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let count = 1;
+  let result = '';
+  for (let i=0; i < str.length; i++){
+    if(str[i] === str[i+1]){
+      count++;
+    } else{
+      result = count === 1 ? result + str[i] : result + count + str[i];
+      count = 1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
